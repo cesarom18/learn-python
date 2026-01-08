819e88dd0136ade3bd9f3f70e48271a387131f40
cesarom18 <cesarosses885@gmail.com>
2026-01-08 12:43:51 -0300

    Introduction completed

---
+++ introduction.py
@@ -0,0 +1,23 @@
+"""
+-= Definitions / Concepts =-
+
+- REPL: Help to comunicate with python interpreter
+in CLI writting instructions or just expresions. The
+meaning of REPL is "Read", "Evaluate", "Print" and "Loop".
+
+- How does python execute the code?: Well, python is a interpreted
+programming language so the interpreter read line by line (From top
+to bottom) and execute each instruction, if there is any error
+the interpreter will show a error in the terminal. Python execute
+the code in this order, first compile our code and transform it in bytecode, second the bytecode is give it to PVM (Python Virtual Machine) and
+last the result of PVM is transformed to machine language.
+"""
+
+# Basic Operators
+add = 1 + 1 # Add two numbers
+sub = 1 - 1 # Substract two numbers
+mul = 1 * 1 # Multiply two numbers
+div = 1 / 1 # Divide two numbers (With decimals)
+div_complete = 1 // 2 # Divide two numbers (Without decimals) 
+rest = 4 % 2 # Module (Rest) of two numbers
+pow = 2 ** 2 # Raise to the power X
