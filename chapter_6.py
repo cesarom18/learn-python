e87723132b44fdd62c00c74716e6cfec31f7263f
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-04 17:43:38 -0300

    Create class section complete

--- chapter_6.py
+++ chapter_6.py
@@ -6,5 +6,14 @@ we declare constructor and methods.
 
 - Object: Is a class instance, to initialize a object we need to assign class contructor
 to a variable and this brings all methods/properties that we delcare inside the class.
+
+- Method: Function inside of the class.
 """
 
+# Create class
+class Car:
+    def turn_on(self): # Declare method
+        print("Car turned on")
+
+my_car = Car() # Create object/instance class
+print(isinstance(my_car, Car)) # Check if object is a instance of the class (Return boolean)
