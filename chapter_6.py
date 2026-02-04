7e548d99cf35b76c1f4ab594b7e9a473e4d0406a
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-04 18:29:29 -0300

    Classs method section complete

--- chapter_6.py
+++ chapter_6.py
@@ -14,6 +14,12 @@ to a variable and this brings all methods/properties that we delcare inside the 
 - self: Is a reserved word that refer to object himself, that means every instance have a
 different "self" to make every object unique in terms of memory location and logic of course.
 In simple words is the same "variable" that we bring the class at the beginning.
+
+- cls: Work in the same way as "self" but in this time the reference is for the class.
+
+- Factory Method: Is a class method that help us to create multiple instance from class
+with default values that we defined inside of the factory method, so we dont have to give
+everytime the instance properties for the constructor.
 """
 
 # Create class
@@ -23,10 +29,21 @@ class Car:
     def __init__(self, brand): # Create constructor
         self.brand = brand # Declare instance propertie
 
-    def turn_on(self): # Declare method
+    @classmethod
+    def factory(cls): # Declare factory method
+        return cls("Chevrolet")
+    
+    @classmethod
+    def turn_on(cls): # Declare class method
         print("Car turned on")
+    
+    def open_door(self): # Declar instance method
+        print("Door opened")
 
 my_car = Car("Toyota") # Create object/instance class
+my_car.open_door() # Call instance method
 print(isinstance(my_car, Car)) # Check if object is a instance of the class (Return boolean)
 print(my_car.brand) # Get instance propertie value
 print(Car.country, my_car.country) # Get class propertie value (From class and from instance)
+Car.turn_on() # Call class method
+factory_car = Car.factory() # Create a instance with factory method
