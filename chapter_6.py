f78bf9efb7e8aebd3d646524a3d7dac489d7d330
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-04 17:53:27 -0300

    Constructor section complete

--- chapter_6.py
+++ chapter_6.py
@@ -8,12 +8,20 @@ we declare constructor and methods.
 to a variable and this brings all methods/properties that we delcare inside the class.
 
 - Method: Function inside of the class.
+
+- Constructor: Is a function that is executed every time we create a instance from class
+
+- self: Is a reserved word that refer to object himself, that means every instance have a
+different "self" to make every object unique in terms of memory location and logic of course.
 """
 
 # Create class
 class Car:
+    def __init__(self, brand): # Create constructor
+        pass
+
     def turn_on(self): # Declare method
         print("Car turned on")
 
-my_car = Car() # Create object/instance class
+my_car = Car("Toyota") # Create object/instance class
 print(isinstance(my_car, Car)) # Check if object is a instance of the class (Return boolean)
