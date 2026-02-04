203b9b3bd25eae3b8f420112d4e86c2a18060092
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-04 18:04:28 -0300

    Propertie class section complete

--- chapter_6.py
+++ chapter_6.py
@@ -18,11 +18,15 @@ In simple words is the same "variable" that we bring the class at the beginning.
 
 # Create class
 class Car:
+    country = "USA" # Declare class propertie
+
     def __init__(self, brand): # Create constructor
-        self.brand = brand # Declare class propertie
+        self.brand = brand # Declare instance propertie
 
     def turn_on(self): # Declare method
         print("Car turned on")
 
 my_car = Car("Toyota") # Create object/instance class
 print(isinstance(my_car, Car)) # Check if object is a instance of the class (Return boolean)
+print(my_car.brand) # Get instance propertie value
+print(Car.country, my_car.country) # Get class propertie value (From class and from instance)
