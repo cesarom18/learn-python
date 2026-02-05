485a4f2e669cf67788b1331d5e9f37cebbb70542
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-05 16:19:03 -0300

    Private propertie and metod section complete

--- chapter_6.py
+++ chapter_6.py
@@ -20,30 +20,38 @@ In simple words is the same "variable" that we bring the class at the beginning.
 - Factory Method: Is a class method that help us to create multiple instance from class
 with default values that we defined inside of the factory method, so we dont have to give
 everytime the instance properties for the constructor.
+
+- Private Properties: This properties can only be accessed within class or
+method, not from outside.
 """
 
 # Create class
 class Car:
     country = "USA" # Declare class propertie
 
-    def __init__(self, brand): # Create constructor
+    def __init__(self, brand, n_wheels): # Create constructor
         self.brand = brand # Declare instance propertie
+        self.__n_wheels = n_wheels # Declare private propertie (Only accessible inside of the class or method)
 
     @classmethod
     def factory(cls): # Declare factory method
-        return cls("Chevrolet")
+        return cls("Chevrolet", 4)
     
     @classmethod
     def turn_on(cls): # Declare class method
         print("Car turned on")
     
-    def open_door(self): # Declar instance method
+    def open_door(self): # Declare instance method
         print("Door opened")
+    
+    def __example_private(self): # Declare private method
+        print(f"Private: {self.__n_wheels}") # Access to private propertie
 
-my_car = Car("Toyota") # Create object/instance class
+my_car = Car("Toyota", 4) # Create object/instance class
 my_car.open_door() # Call instance method
 print(isinstance(my_car, Car)) # Check if object is a instance of the class (Return boolean)
 print(my_car.brand) # Get instance propertie value
 print(Car.country, my_car.country) # Get class propertie value (From class and from instance)
 Car.turn_on() # Call class method
 factory_car = Car.factory() # Create a instance with factory method
+print(my_car.__dict__) # Get all properties from instance
