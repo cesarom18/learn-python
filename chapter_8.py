342c740f5a3c4c10cf4a43d0352d46cddcb1255b
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-11 16:53:38 -0300

    Compiled modules section complete

--- chapter_8.py
+++ chapter_8.py
@@ -3,4 +3,7 @@
 
 - Modules: Related parts of code, functions or variables that can be exported
 to other files in a application.
+
+- Module cache (__pycache__): This folder contains all pre-compiled code of all python modules created to improve
+the app performance, if the module change his modify date then python will replace the module cache again.
 """
