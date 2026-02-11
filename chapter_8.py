2d17c8d9e6c22a4c720469ea0be2b64a11359320
cesarom18 <98192523+cesarom18@users.noreply.github.com>
2026-02-11 17:48:30 -0300

    Packages section complete

--- chapter_8.py
+++ chapter_8.py
@@ -6,4 +6,7 @@ to other files in a application.
 
 - Module cache (__pycache__): This folder contains all pre-compiled code of all python modules created to improve
 the app performance, if the module change his modify date then python will replace the module cache again.
+
+- Package: The main difference between module and package is that package is a do reference to a folder,
+to transform a folder into a package we need to create a "__init__.py" file.
 """
