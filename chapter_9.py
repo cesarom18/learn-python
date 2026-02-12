5659b327a3dc6bf7e3d26a2caaee2341f0c5f299
cesarom18 <cesarosses885@gmail.com>
2026-02-12 18:37:19 -0300

    Chapter 9 complete

--- chapter_9.py
+++ chapter_9.py
@@ -2,21 +2,21 @@
 -= Definitions / Concepts
 """
 
-from pathlib import Path 
+from pathlib import Path
 
 # Refer to path with raw string (Ignore backlash for escape characters)
-path = Path("chapter_8.py") # Create path (Is only a refer, it can exists or not)
-home_path = Path.home() # Return home path
-print(path.is_file()) # Check if path is file (Return boolean)
-print(path.is_dir()) # Check if path is directory (Return boolean)
-print(path.exists()) # Check if path exists (Return boolean)
+path = Path("chapter_8.py")  # Create path (Is only a refer, it can exists or not)
+home_path = Path.home()  # Return home path
+print(path.is_file())  # Check if path is file (Return boolean)
+print(path.is_dir())  # Check if path is directory (Return boolean)
+print(path.exists())  # Check if path exists (Return boolean)
 print(
-        path.name, # Return filename with extension
-        path.stem, # Return filename
-        path.suffix, # Return extension
-        path.parent, # Return parent directory
-        path.absolute() # Return absolute path
+    path.name,  # Return filename +  extension
+    path.stem,  # Return filename
+    path.suffix,  # Return extension
+    path.parent,  # Return parent directory
+    path.absolute(),  # Return absolute path
 )
-print(path.with_name("new_chapter_8.py")) # Change filename with extension
-print(path.with_suffix(".js")) # Change extension
-print(path.with_stem("new_chapter")) # Change filename
+print(path.with_name("new_chapter_8.py"))  # Change filename with extension
+print(path.with_suffix(".js"))  # Change extension
+print(path.with_stem("new_chapter"))  # Change filename
