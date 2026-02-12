ad6c6966aa190f6ec023075c0d78b1532fe13726
cesarom18 <cesarosses885@gmail.com>
2026-02-12 18:49:15 -0300

    Manage files introduction section complete

---
+++ chapter_10.py
@@ -0,0 +1,10 @@
+"""
+-= Definitions / Concepts
+"""
+from pathlib import Path
+
+file = Path("chapter_9.py") # Get file
+print(file.exists()) # Check if file exists
+print(file.rename("new_chapter_9.py")) # Rename file
+print(file.unlink()) # Remove file
+print(file.stat()) # Get file metadata (Size, access date, modify date)
