afddd27b600326eee46c308a0bf50771a419a191
cesarom18 <cesarosses885@gmail.com>
2026-02-12 18:53:11 -0300

    Write and read files section complete

--- chapter_10.py
+++ chapter_10.py
@@ -8,3 +8,5 @@ print(file.exists()) # Check if file exists
 print(file.rename("new_chapter_9.py")) # Rename file
 print(file.unlink()) # Remove file
 print(file.stat()) # Get file metadata (Size, access date, modify date)
+print(file.read_text("utf-8")) # Read text from file
+file.write_text("example", "utf-8") # Write file (Text, encoding)
