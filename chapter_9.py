f3dc93f886023ae172b51e30769dc4c514a9bfc1
cesarom18 <cesarosses885@gmail.com>
2026-02-13 23:42:37 -0300

    Chapter 9 complete

--- chapter_9.py
+++ chapter_9.py
@@ -2,7 +2,8 @@
 -= Definitions / Concepts
 """
 
-from pathlib import Path
+from pathlib import Path # Import Path
+from io import open # Import open
 
 # Refer to path with raw string (Ignore backlash for escape characters)
 path = Path("chapter_8.py")  # Create path (Is only a refer, it can exists or not)
@@ -20,3 +21,11 @@ print(
 print(path.with_name("new_chapter_8.py"))  # Change filename with extension
 print(path.with_suffix(".js"))  # Change extension
 print(path.with_stem("new_chapter"))  # Change filename
+file = open("chapter_8.py", "w") # Open file in write mode (If file doesn't not exist, python will create)
+file.write("example") # Write file (We need to save it)
+file.close() # Close and save the file with changes
+file_2 = open("chapter_9.py", "r") # Open file in read mode (Default mode)
+content = file_2.read() # Read file
+content_list = file_2.readlines() # Read text and separate it in lines inside of list
+with open("chapter_1.py") as file_3: # Use with statement (Will close the file if an error appear or we execute successfully our logic)
+    print("File open")
